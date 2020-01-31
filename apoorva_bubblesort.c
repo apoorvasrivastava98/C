1a72af861e07421a0318ea8960f290c74fd7db05
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-01-31 23:35:11 +0530

    Add files via upload

---
+++ apoorva_bubblesort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int a[5],i,j,n,t;
+printf("Enter the elements in array");
+for(i=0;i<5;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<5;i++)
+{
+for(j=0;j<5;j++)
+{
+if(a[i]<a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}}
+for(i=0;i<5;i++)
+{
+printf("%d",a[i]);
+}
+}
