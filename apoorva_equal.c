be5d9de597e5daf8b3c4c7a19e7f37eb81ccb4e7
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-02-11 23:10:16 +0530

    Add files via upload

---
+++ apoorva_equal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2;
+printf("Enter Number1 amd Number2");
+scanf("%d%d",&n1,&n2);
+if(n1==n2)
+{
+printf("Number1 equals to Number2");
+}
+else
+{
+printf("Number1 is not equal to Number2");
+}
+}
